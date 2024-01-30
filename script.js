af251a8e19541a7f67c2e35a78e694c906cf83bf
ahwhitcomb <150101691+ahwhitcomb@users.noreply.github.com>
2024-01-29 19:43:01 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Example of simple form validation
+document.addEventListener('DOMContentLoaded', function() {
+    const flightForm = document.querySelector('#flights form');
+    flightForm.addEventListener('submit', function(event) {
+        // Perform validation
+        // If invalid, call event.preventDefault();
+    });
+
+    // Similarly for hotel form
+});
